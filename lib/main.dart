23411663ac36439acf8eb5e1d0f17d3fe173c932
Iskandar Zulqarnain Hilmi <iskandarzhilmi@gmail.com>
2022-10-02 03:51:56 +0800

    feat: installed firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:voxalate/firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -16,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Voxalate',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -37,6 +42,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final FlutterSoundRecorder recorder = FlutterSoundRecorder();
   final FlutterSoundPlayer player = FlutterSoundPlayer();
+  final storageReference = FirebaseStorage.instance.ref();
 
   @override
   void initState() {
@@ -65,16 +71,55 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             TextButton(
               onPressed: () async {
-                await recorder.startRecorder(
-                  toFile: 'test.m4a',
-                  codec: Codec.aacMP4,
-                );
+                if (Platform.isIOS) {
+                  final file = File('recording.m4a');
+                  await recorder.startRecorder(
+                    toFile: file.path,
+                    codec: Codec.aacMP4,
+                  );
+                } else if (Platform.isAndroid) {
+                  final file = File('recording.mp3');
+                  await recorder.startRecorder(
+                    toFile: file.path,
+                    codec: Codec.mp3,
+                  );
+                } else {
+                  throw UnsupportedError('Unsupported platform');
+                }
               },
               child: const Text('Start recording'),
             ),
             TextButton(
               onPressed: () async {
                 await recorder.stopRecorder();
+
+                String? fileType;
+
+                if (Platform.isIOS) {
+                  fileType = 'm4a';
+                } else if (Platform.isAndroid) {
+                  fileType = 'mp3';
+                } else {
+                  throw UnsupportedError('Unsupported platform');
+                }
+
+                // final file = await File('test.$fileType').create();
+                // final uploadTask = storageReference
+                //     .child('speech/test.$fileType')
+                //     .putFile(file);
+                // await uploadTask.whenComplete(() => log('File uploaded'));
+
+                final file = File('test.$fileType');
+                final bytes = await file.readAsBytes();
+                final uploadTask = storageReference
+                    .child('test.$fileType')
+                    .putData(bytes,
+                        SettableMetadata(contentType: 'audio/$fileType'));
+
+                final snapshot = await uploadTask;
+                final url = await snapshot.ref.getDownloadURL();
+
+                log('Uploaded file to $url');
               },
               child: const Text('Stop recording'),
             ),
