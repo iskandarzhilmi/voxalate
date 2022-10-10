343c363cc7a5cbc84a7f25f86469938be299641d
Iskandar Zulqarnain Hilmi <iskandarzhilmi@gmail.com>
2022-10-10 17:17:18 +0800

    fix: minor ui change on the home screen.

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -65,7 +65,11 @@ class _HomePageState extends State<HomePage> {
                     const Text(
                       'Tap the button to start recording',
                     ),
-                    TextButton(
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    IconButton(
+                      iconSize: 50,
                       onPressed: () {
                         if (isRecording) {
                           stopRecording();
@@ -73,15 +77,15 @@ class _HomePageState extends State<HomePage> {
                           startRecording();
                         }
                       },
-                      child: isRecording
-                          ? const Text('Stop Recording')
-                          : const Text('Start Recording'),
+                      icon: isRecording
+                          ? const Icon(Icons.stop)
+                          : const Icon(Icons.mic),
                     ),
                   ],
                 ),
               ),
               const SizedBox(
-                height: 100,
+                height: 50,
               ),
               BlocBuilder<TranscribeBloc, TranscribeState>(
                 builder: (context, state) {
@@ -92,8 +96,13 @@ class _HomePageState extends State<HomePage> {
                   } else if (state is TranscribeLoaded) {
                     return Column(
                       children: [
-                        Text(state.transcribeOutput.transcription),
-                        Text(state.transcribeOutput.translation ?? ''),
+                        SelectableText(state.transcribeOutput.transcription),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        SelectableText(
+                          state.transcribeOutput.translation ?? '',
+                        ),
                         TextButton(
                           onPressed: () {
                             setState(() {
@@ -114,6 +123,9 @@ class _HomePageState extends State<HomePage> {
                     return const Text('Something went wrong');
                   }
                 },
+              ),
+              const SizedBox(
+                height: 100,
               ),
             ],
           ),
@@ -185,184 +197,7 @@ class _HomePageState extends State<HomePage> {
     });
     final path = await recorder.stopRecorder();
     if (!mounted) return;
+
     context.read<TranscribeBloc>().add(TranscribeStarted(path!));
-    // log('Recording stopped at $path');
-    // final file = File(path!);
-    // final bytes = await file.readAsBytes();
-    // final snapshot = await storageReference.child('test.wav').putData(bytes);
-    // log('Uploaded file to ${snapshot.ref.fullPath}');
-    // log('Download URL: ${await snapshot.ref.getDownloadURL()}');
-
-    final PredictionModel prediction = await startPrediction();
-
-    setState(() {
-      predictionId = prediction.id;
-    });
-  }
-
-  Future<PredictionModel> startPrediction() async {
-    final String modelVersion = await getModelVersion();
-    final String downloadUrl = await getDownloadUrl();
-    final String body = jsonEncode({
-      'version': modelVersion,
-      'input': {
-        'audio': downloadUrl,
-        'translate': true,
-        'model': 'large',
-      },
-    });
-    final response = await http.post(
-      Uri.parse('https://api.replicate.com/v1/predictions'),
-      headers: {
-        'Authorization': 'Token 066709f72f2494c995b2585a0776d2b7c49287ae',
-        'Content-Type': 'application/json',
-      },
-      body: body,
-    );
-    log('Response: ${response.body}');
-    return PredictionModel.fromJson(
-      jsonDecode(response.body) as Map<String, dynamic>,
-    );
-  }
-
-  Stream<PredictionModel> getPredictionStream(String predictionId) async* {
-    while (true) {
-      final response = await http.get(
-        Uri.parse(
-          'https://api.replicate.com/v1/predictions/$predictionId',
-        ),
-        headers: {
-          'Authorization': 'Token 066709f72f2494c995b2585a0776d2b7c49287ae',
-        },
-      );
-      final predictionOutput = PredictionModel.fromJson(
-        // utf decode to fix issue with special characters
-        jsonDecode(utf8.decode(response.bodyBytes)) as Map<String, dynamic>,
-      );
-      yield predictionOutput;
-      if (predictionOutput.status == 'succeeded') {
-        break;
-      }
-      await Future.delayed(const Duration(seconds: 1));
-    }
-  }
-
-  Future<OpenAiCompletionModel> getSummary(String englishText) async {
-    final String body = jsonEncode({
-      // 'model': 'text-davinci-002',
-      'model': 'text-curie-001',
-      'max_tokens': 1500,
-      'n': 1,
-      'prompt': '$englishText\nSummarise this:\n',
-    });
-    final response = await http.post(
-      Uri.parse('https://api.openai.com/v1/completions'),
-      headers: {
-        'Authorization':
-            'Bearer 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',
-        'Content-Type': 'application/json',
-      },
-      body: body,
-    );
-    log('Response: ${response.body}');
-    return OpenAiCompletionModel.fromJson(
-      jsonDecode(response.body) as Map<String, dynamic>,
-    );
-  }
-
-  Future<String> getDownloadUrl() async {
-    return storageReference.child('test.wav').getDownloadURL();
-  }
-
-  Future<String> getModelVersion() async {
-    final response = await http.get(
-      Uri.parse('https://api.replicate.com/v1/models/openai/whisper/versions'),
-      headers: {
-        'Authorization': 'Token 066709f72f2494c995b2585a0776d2b7c49287ae',
-        'Content-Type': 'application/json',
-      },
-    );
-    final Map<String, dynamic> json =
-        jsonDecode(response.body) as Map<String, dynamic>;
-    final results = json['results'] as List<dynamic>;
-    final latestVersion = results.first as Map<String, dynamic>;
-    return latestVersion['id'] as String;
   }
 }
-
-
-// if (predictionId != null)
-//                       StreamBuilder<PredictionModel>(
-//                         stream: getPredictionStream(predictionId!),
-//                         builder: (context, snapshot) {
-//                           if (snapshot.hasData) {
-//                             if (snapshot.data?.output?.translation != null) {
-//                               return Column(
-//                                 children: [
-//                                   SelectableText(
-//                                     snapshot.data!.output!.transcription!,
-//                                   ),
-//                                   const SizedBox(
-//                                     height: 10,
-//                                   ),
-//                                   SelectableText(
-//                                     snapshot.data!.output!.translation!,
-//                                   ),
-//                                   TextButton(
-//                                     child: isShowSummary
-//                                         ? const Text('Hide')
-//                                         : const Text('Show'),
-//                                     onPressed: () {
-//                                       setState(() {
-//                                         isShowSummary = !isShowSummary;
-//                                       });
-//                                     },
-//                                   ),
-//                                   if (isShowSummary)
-//                                     FutureBuilder<OpenAiCompletionModel>(
-//                                       future: getSummary(
-//                                         snapshot.data!.output!.translation!,
-//                                       ),
-//                                       builder: (context, snapshot) {
-//                                         if (snapshot.hasData) {
-//                                           return SelectableText(
-//                                             snapshot.data!.choices?[0].text ??
-//                                                 'null',
-//                                           );
-//                                         }
-//                                         return const SizedBox();
-//                                       },
-//                                     ),
-//                                 ],
-//                               );
-//                             }
-//                             if (snapshot.data?.output?.transcription != null) {
-//                               return Column(
-//                                 children: [
-//                                   SelectableText(
-//                                     snapshot.data!.output!.transcription!,
-//                                   ),
-//                                   FutureBuilder(
-//                                     future: getSummary(
-//                                       snapshot.data!.output!.transcription!,
-//                                     ),
-//                                     builder: (context, snapshot) {
-//                                       if (snapshot.hasData) {
-//                                         return SelectableText(
-//                                           (snapshot.data as String?) ?? 'null',
-//                                         );
-//                                       }
-//                                       return const Text('Loading...');
-//                                     },
-//                                   ),
-//                                 ],
-//                               );
-//                             }
-//                             return const Text('Loading...');
-//                           } else {
-//                             return const Text('No data');
-//                           }
-//                         },
-//                       )
-//                     else
-//                       Container(),
